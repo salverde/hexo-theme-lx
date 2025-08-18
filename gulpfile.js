794d0f55f2518fa2a17f88650191311d2e495a22
s13e <sa1omon@icloud.com>
2025-08-18 10:32:47 -0600

    update gulp packages / fix custom css inclusion

--- gulpfile.js
+++ gulpfile.js
@@ -1,75 +1,92 @@
 var gulp = require("gulp"),
-    del = require("del"),
-    stylus = require("gulp-stylus"),
-    prefix = require("gulp-autoprefixer"),
-    rename = require("gulp-rename"),
-    uglify = require("gulp-uglify"),
-    imagemin = require("gulp-imagemin");
+	del = require("del"),
+	stylus = require("gulp-stylus"),
+	prefix = require("gulp-autoprefixer"),
+	rename = require("gulp-rename"),
+	uglify = require("gulp-uglify"),
+	imagemin = require("gulp-imagemin");
 
-function clean(cb){
-  const deletedPaths = del.sync(["source/dist/*"]);
-  cb();
-}
-
-function cssMinify(){
-  gulp.src("source/css/main.styl")
-    .pipe(rename({
-      suffix: ".min"
-    }))
-    .pipe(stylus({
-      compress: true
-    }))
-    .pipe(gulp.dest("source/dist/css"));
+function clean(cb) {
+	const deletedPaths = del.deleteAsync(["source/dist/*"]);
+	cb();
 }
 
-function cssPrefix(){
-  gulp.src("source/dist/css/main.min.css")
-    .pipe(prefix())
-    .pipe(gulp.dest("source/dist/css"));
+function cssMinify() {
+	gulp
+		.src("source/css/*.styl")
+		.pipe(
+			rename({
+				suffix: ".min",
+			}),
+		)
+		.pipe(
+			stylus({
+				compress: true,
+			}),
+		)
+		.pipe(gulp.dest("source/dist/css"));
 }
 
-function jsMinify(){
-  gulp.src("source/js/*.js")
-    .pipe(rename({
-      suffix: ".min"
-    }))
-    .pipe(uglify())
-    .pipe(gulp.dest("source/dist/js"));
+function cssPrefix() {
+	gulp
+		.src("source/dist/css/*.css")
+		.pipe(prefix())
+		.pipe(gulp.dest("source/dist/css"));
 }
 
-function imageMinify(){
-  gulp.src("source/images/*.jpeg*")
-    .pipe(rename({
-      suffix: ".min"
-    }))
-    .pipe(imagemin([
-      imagemin.mozjpeg({quality:70, progressive: true})
-    ]))
-    .pipe(gulp.dest("source/dist/images"));
-  gulp.src("source/images/*.svg").pipe(gulp.dest("source/dist/images"));
+function jsMinify() {
+	gulp
+		.src("source/js/*.js")
+		.pipe(
+			rename({
+				suffix: ".min",
+			}),
+		)
+		.pipe(uglify())
+		.pipe(gulp.dest("source/dist/js"));
 }
 
-function buildAll(cb){
-  cssMinify();
-  jsMinify();
-  imageMinify();
-  cb();
+function imageMinify() {
+	// import gulp from "gulp";
+	// Or as CommonJS:
+	// const gulp = require("gulp");
+
+	gulp.task("optimize", async () => {
+		const imagemin = (await import("gulp-imagemin")).default;
+
+		return gulp
+			.src("source/images/*.jpeg")
+			.pipe(
+				rename({
+					suffix: ".min",
+				}),
+			)
+			.pipe(imagemin([imagemin.mozjpeg({ quality: 70, progressive: true })]))
+			.pipe(gulp.dest("source/dist/images"));
+	});
 }
-function minImg(cb){
-  imageMinify();
-  cb();
+
+function buildAll(cb) {
+	cssMinify();
+	jsMinify();
+	imageMinify();
+	cb();
 }
-function minCss(cb){
-  cssMinify();
-  cb();
+function minImg(cb) {
+	imageMinify();
+	cb();
 }
-function minJs(cb){
-  jsMinify();
-  cb();
+function minCss(cb) {
+	cssMinify();
+	cb();
 }
-function prefixCss(cb){
-  cssPrefix();
-  cb();
+function minJs(cb) {
+	jsMinify();
+	cb();
+}
+function prefixCss(cb) {
+	cssPrefix();
+	cb();
 }
 exports.default = buildAll;
 exports.clean = clean;
